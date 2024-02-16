e23d43e83a7a741e2205e5d43eb8dce94ab1df5c
Srujam-Dave <156384161+Srujam-Dave@users.noreply.github.com>
2024-02-15 17:45:10 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
@@ -9,6 +10,11 @@ class IsMoon implements StringChecker {
   }
 }
 
+class StringCheckerTest implements StringChecker {
+  public boolean checkString(String s) {
+    return true;
+  }
+}
 public class TestListExamples {
   @Test(timeout = 500)
   public void testMergeRightEnd() {
@@ -17,5 +23,18 @@ public class TestListExamples {
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+  @Test
+  public void testStringChecker() {
+
+    ArrayList<String> outputList = new ArrayList<String>();
+    outputList.add("moon");
+
+    ArrayList<String> inputList = new ArrayList<String>();
+    inputList.add("moon");
+    inputList.add("hi");
+
+    assertEquals(outputList, ListExamples.filter(inputList, new IsMoon()));
   }
 }
