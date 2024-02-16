c55de3be3e7c2aa6a0d5075e94667862013b6f89
Srujam-Dave <156384161+Srujam-Dave@users.noreply.github.com>
2024-02-15 17:44:28 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,42 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "File Found"
+else
+    echo "File not found"
+    exit
+fi
+
+cp TestListExamples.java grading-area
+cp student-submission/ListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]]
+then
+    echo "womp womp ur code didn't compile. See error above"
+    exit
+else
+    echo "Running Program."
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-output.txt
+
+testline=$(cat test-output.txt | tail -n 2)
+
+if [[ $testline == *"Failures"* ]]
+then
+    numbers=$(echo $testline | grep -oE '[0-9]+' | sed 'N;s/\n/:/')
+    tests_run=$(echo "$numbers" | awk -F: '{print $1}')
+    failures=$(echo "$numbers" | awk -F: '{print $2}')
+else
+    tests_run=$(echo $testline | grep -oE '[0-9]+' | sed 'N;s/\n/:/')    
+    failures=0
+fi
+
+successes=$((tests_run - failures))
+echo "Tests ran. Your score is $successes / $tests_run"
